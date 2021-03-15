e11be16f34e4d3422d7ee2ece5504d2de117bf40
Yurii <baziuk.u@gmail.com>
2021-03-15 23:10:19 +0200

    'first_commit'

--- src/app/pet-projects/todo-list/todo-list.component.ts
+++ src/app/pet-projects/todo-list/todo-list.component.ts
@@ -3,7 +3,6 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 interface Msgs {
   text: string
-  isLiked: boolean
   isDone: boolean
   isImportant: boolean
   postDate: Date
@@ -35,10 +34,6 @@ export class TodoListComponent implements OnInit {
     this.arr.splice(i, 1);
   }
 
-  toggleLike(i) {
-    this.arr[i].isLiked = !this.arr[i].isLiked;
-  }
-
   toggleImportant(i) {
     this.arr[i].isImportant = !this.arr[i].isImportant;
     this.arr[i].isDone = false
@@ -53,13 +48,11 @@ export class TodoListComponent implements OnInit {
     if (this.todoForm.value.inputField.trim()){
       let msg: Msgs = {
         text: this.todoForm.value.inputField,
-        isLiked: false,
         isDone: false,
         isImportant: false,
         postDate: new Date()
       }
       this.todoForm.reset()
-      // msg.text = this.inputF;
       this.arr.push(msg);
     }
   }
